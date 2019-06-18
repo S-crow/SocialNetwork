751240b0e500de876559621b42106361e27680bf
S-crow <nicolas75.besse@sfr.fr>
2019-06-18 18:04:19 +0200

    tryc onnexion bdd

--- index.php
+++ index.php
@@ -1,32 +1,41 @@
 		<html>
 		<head>
-			<title>Bienvenue sur le réseau social</title>
+			<title>Sl-hack</title>
 			<link rel="stylesheet" href="style.css">
+			<meta http-equiv="Content-Type" content="text/html, charset=UTF-8" />
 		</head>
 		<body>
-        <?php
-		$pdo = new PDO("mysql:host=localhost:8889;dbname=mydb", "root", "root");
-		if(isset($_POST['pseudo']) && isset($_POST['passwd'])) {
-            $pseudo = $_POST["pseudo"];
-            $req = "SELECT passwd from users where pseudo='".$pseudo."'";
-            $passbase = $pdo->query($req)->fetch();
-            $passbase = $passbase[0];
-            var_dump($passbase);
-			$passEntered = $_POST["passwd"];
-			if ($passEntered == $passbase) {
-				echo "success";
-				setCookie("user", $pseudo);
-				header("Location: profile.php?pseudo=".$pseudo);
-			}
-		} 
-		if(isset($_POST['newpseudo']) && isset($_POST['newpasswd'])) {
-			$valeurs = ["pseudo" => $_POST["newpseudo"], "passwd" => $_POST["newpasswd"]];
-			$req = "INSERT INTO users (pseudo, passwd) VALUES (:pseudo, :passwd)";
-			$req = $pdo->prepare($req);
-			$req->execute($valeurs);
-		} 
-		?>
-		<h1>Réseau social Sl-hack</h1>
+<?php
+    try {
+	$pdo = new PDO("mysql:host=localhost;dbname=mabasedb", "Scrow", "Scrow");
+
+    } catch (PDOException $e) {
+	print "Error connnection failed : ". $e->getMessage() . "<br/>";
+	die();
+    }
+
+	if(isset($_POST['pseudo']) && isset($_POST['passwd'])) {
+		$pseudo = $_POST["pseudo"];
+		$req = "SELECT passwd from users where pseudo='".$pseudo."'";
+		$passbase = $pdo->query($req)->fetch();
+		$passbase = $passbase[0];
+		var_dump($passbase);
+		$passEntered = $_POST["passwd"];
+		if ($passEntered == $passbase) {
+			echo "success";
+			setCookie("user", $pseudo);
+			header("Location: profile.php?pseudo=".$pseudo);
+		}
+	} 
+    if(isset($_POST['newpseudo']) && isset($_POST['newpasswd'])) {
+	    echo "ok";
+	    $valeurs = ['pseudo' => $_POST["newpseudo"], 'passwd' => $_POST["newpasswd"]];
+		$req = "INSERT INTO users (pseudo, passwd) VALUES (:pseudo, :passwd)";
+		$req = $pdo->prepare($req);
+		$req->execute($valeurs);
+	} 
+?>
+		<h1>Réseau social</h1>
 		<p>Connectez vous</p>
 		<form method="post" action="index.php">
 		<input type="text" name="pseudo" value="" />
